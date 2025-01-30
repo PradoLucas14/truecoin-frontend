8ab95e20c8811f62f7e2cc0db8dd3a58224ac4fd
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-01-30 11:42:33 -0300

    modifique los campos del usario harcodiado

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,8 +1,8 @@
 //Trabajar logica aqui
 // Trabajer la logica y declarar el usuario
 const userAdmin = {
-    email: "admin@fitfactory.com",
-    password: "Fitfac1234"
+    email: "admin@ttruecoin2025.com",
+    password: "TrueCOIN1234"
 }
 
 export const login = (usuario) => {
