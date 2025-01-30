7b43d6785dcec5ee24510402635a71dd0959d109
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-01-30 11:23:51 -0300

    agregue el import para el componente Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/home/Home'
+import Register from './pages/register/Register'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <Home/>
+      <Register></Register>
     </>
   )
 }
