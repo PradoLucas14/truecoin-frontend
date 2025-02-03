4d01e19e7c87f0f9b41a9ef8743eed39577e9a47
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-03 12:02:23 -0300

    agregue maquetado para el componente Menu

--- src/components/commun/menu/Menu.jsx
+++ src/components/commun/menu/Menu.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from '../../../assets/Logo.png'
+import moneda from'../../../assets/moneda.png'
 
 const Menu = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+     <Navbar expand="lg" className="p-0 m-0">
+      <Container fluid>
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt="" width={130} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Nosotros</Nav.Link>
+            <NavDropdown title="Categorias" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href=''>Login</Nav.Link>
+            <Nav.Link href="#link">
+             
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar> 
+    </>
   )
 }
 
