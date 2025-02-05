087f44afca510455364cf16f93f8fb98f4833ffc
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-05 11:38:22 -0300

    agregue el maquetado y estilos necesarios para el componente Categories

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from '../../components/homeComponents/hero/hero/Hero'
 import Register from '../register/Register'
+import Categories from '../../components/homeComponents/hero/categories/categories'
 
 function Home() {
   return (
     <div>
       <Hero/>
-      
+      <Categories></Categories>
     </div>
   )
 }
