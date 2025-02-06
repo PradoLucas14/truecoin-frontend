18f5eaf1e707a3fddca69231eca26a8d8a0b30c4
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-06 11:00:47 -0300

    agregue el maquetado necesario para el componente CardProductos

--- src/components/productos/cardProductos/CardProductos.jsx
+++ src/components/productos/cardProductos/CardProductos.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Row, Col, Card, Button, Container } from 'react-bootstrap'
+import Moneda from'../../../assets/moneda.png'
 
 const CardProductos = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Row>
+        <Col sm={6} md={4} lg={3} className='pb-3'>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="https://images.pexels.com/photos/10202985/pexels-photo-10202985.jpeg?auto=compress&cs=tinysrgb&w=400" />
+            <Card.Body>
+              <Card.Title> 1Kg Pan</Card.Title>
+              <Card.Text>
+                El producto de nuestra pnaderia fue realizado en horno de barro.
+              </Card.Text>
+              <Card.Text>
+                <img src={Moneda} alt="" width={50} className='m-0'/> 6000
+              </Card.Text>
+              <Button variant="dark">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from '../../components/homeComponents/hero/hero/Hero'
 import Register from '../register/Register'
 import Categories from '../../components/homeComponents/hero/categories/categories'
+import CardProductos from '../../components/productos/cardProductos/CardProductos'
+import { Row } from 'react-bootstrap'
 
 function Home() {
   return (
     <div>
       <Hero/>
       <Categories></Categories>
+      <CardProductos></CardProductos>
+      
     </div>
   )
 }
