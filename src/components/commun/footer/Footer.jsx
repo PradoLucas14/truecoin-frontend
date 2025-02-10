1536b8e12cc9082d189c880ad44a42bcf49035ef
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-10 12:01:10 -0300

    realize un acomodo del codigo en documento footer.css

--- src/components/commun/footer/Footer.jsx
+++ src/components/commun/footer/Footer.jsx
@@ -3,16 +3,18 @@ import './footer.css'
 import logo from '../../../assets/Logo.png'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Col, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
 
 const Footer = () => {
   return (
     <div className='bgFooter text-center'>
       <img src={logo} alt="" width={250} className='imgLogo' />
       <div className='d-md-flex d-lg-flex gap-3 justify-content-center pb-4'>
-        <p className='text-white linksFooter'>Home</p>
-        <p className='text-white linksFooter'>Nosotros</p>
-        <p className='text-white linksFooter'>Productos</p>
-        <p className='text-white linksFooter'>Login</p>
+        <NavLink to={"/"}><p className='text-white linksFooter'>Home</p></NavLink>
+       <NavLink> <p className='text-white linksFooter'>Nosotros</p></NavLink>
+        <NavLink><p className='text-white linksFooter'>Productos</p></NavLink>
+        <NavLink><p className='text-white linksFooter'>Login</p></NavLink>
       </div>
       <div className='pb-2 d-flex gap-4 justify-content-center'>
         <p className='text-white btnRedondo'><i class="bi bi-instagram"></i></p>
