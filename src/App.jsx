3a02029942648177d64f5705e1a78a1b3b624ccf
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-10 11:28:33 -0300

    cree las rutas para las opciones del menu login, Home, Nosotros y Productos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Register from './pages/register/Register'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Menu from'./components/commun/menu/Menu'
 import Footer from './components/commun/footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const usuario = JSON.parse(sessionStorage.getItem("UsuarioTrueCoin")) || "";
@@ -13,10 +14,21 @@ function App() {
 
   return (
     <>
-    
-      <Menu></Menu>
-      <Home></Home>
-      <Footer></Footer>
+      <BrowserRouter>
+        <Menu></Menu>
+
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/Login' element={<Register></Register>}></Route>
+          {/* <Route path='/Nosotros' element={<Nosotros></Nosotros>}></Route> */}
+          <Route path='/Productos'></Route>
+        </Routes>
+
+        <Footer></Footer>
+      </BrowserRouter>
+      
+      
+      
       {/* <Register setUsuarioLogueado={setUsuarioLogueado}></Register> */}
     </>
   )
