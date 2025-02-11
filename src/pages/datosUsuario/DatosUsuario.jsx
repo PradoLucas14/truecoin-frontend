9a601db1936ceee3ae6960745c482c2771d2534e
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-11 11:58:32 -0300

    agregue el maquetado necesario para el nombre de usuario y la img del usuario

--- src/pages/datosUsuario/DatosUsuario.jsx
+++ src/pages/datosUsuario/DatosUsuario.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import './datosUsuarios.css'
 
 const DatosUsuario = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='container-fluid py-3'>
+            <div className='d-flex flex-colum justify-content-center'>
+                <div className=''>
+                    <p className=''>Caserez Laura</p>
+                    <img src="https://images.pexels.com/photos/8558908/pexels-photo-8558908.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Img del usuario" className='imgPerfilUsuario' />
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
 }
 
 export default DatosUsuario
