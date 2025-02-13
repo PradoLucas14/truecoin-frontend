d7182a22400cef12bc52be52dc117110f898903e
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-13 09:44:48 -0300

    modifique el color del titulo (Gestionar Producto) a black, modifique los nombres de los th

--- src/pages/administrador/Administrador.jsx
+++ src/pages/administrador/Administrador.jsx
@@ -10,7 +10,7 @@ const Administrador = () => {
     <div>
       <section className="container mainSection ">
         <div className="d-flex justify-content-between align-items-center mt-5 py-3">
-          <h1 className="display-5 text-white">Gestionar Clases</h1>
+          <h1 className="display-6 fw-normal">Gestionar Producto</h1>
           
             <i className="bi bi-file-earmark-plus fs-4"></i>
           
@@ -19,10 +19,10 @@ const Administrador = () => {
         <Table responsive  bordered hover>
           <thead>
             <tr className="text-center">
-              <th>Clase</th>
-              <th>Profesor/a</th>
-              <th>Fecha</th>
-              <th>Horario</th>
+              <th>ID</th>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>Descripcion</th>
               <th>URL de Imagen</th>
               <th>Opciones</th>
             </tr>
