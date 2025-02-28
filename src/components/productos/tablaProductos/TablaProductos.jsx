b2a90c000282f52623ec32b584d35b3ae1110f19
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-28 08:51:29 -0300

    agre el maquetado necesario para el componente Tabla Productos y elimine el contenido del componente administrador

--- src/components/productos/tablaProductos/TablaProductos.jsx
+++ src/components/productos/tablaProductos/TablaProductos.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
+import ItemProducto from '../itemProducto/ItemProducto'
 
 const TablaProductos = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <section className="container mainSection ">
+                <div className="d-flex justify-content-between align-items-center mt-5 py-3">
+                    <h1 className="display-6 fw-normal">Gestionar Producto</h1>
+
+                    <i className="bi bi-file-earmark-plus fs-4"></i>
+
+                </div>
+                <hr />
+                <Table responsive bordered hover>
+                    <thead>
+                        <tr className="text-center">
+                            <th>ID</th>
+                            <th>Producto</th>
+                            <th>Precio</th>
+                            <th>Descripcion</th>
+                            <th>URL de Imagen</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <ItemProducto></ItemProducto>
+                        <ItemProducto></ItemProducto>
+                        <ItemProducto></ItemProducto>
+                        <ItemProducto></ItemProducto>
+
+                    </tbody>
+                </Table>
+            </section>
+        </div>
+    )
 }
 
 export default TablaProductos
--- src/pages/administrador/Administrador.jsx
+++ src/pages/administrador/Administrador.jsx
@@ -8,34 +8,7 @@ import { Table } from 'react-bootstrap'
 const Administrador = () => {
   return (
     <div className=''>
-      <section className="container mainSection ">
-        <div className="d-flex justify-content-between align-items-center mt-5 py-3">
-          <h1 className="display-6 fw-normal">Gestionar Producto</h1>
-          
-            <i className="bi bi-file-earmark-plus fs-4"></i>
-          
-        </div>
-        <hr />
-        <Table responsive  bordered hover>
-          <thead>
-            <tr className="text-center">
-              <th>ID</th>
-              <th>Producto</th>
-              <th>Precio</th>
-              <th>Descripcion</th>
-              <th>URL de Imagen</th>
-              <th>Opciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <ItemProducto></ItemProducto>
-            <ItemProducto></ItemProducto>
-            <ItemProducto></ItemProducto>
-            <ItemProducto></ItemProducto>
-
-          </tbody>
-        </Table>
-      </section>
+      
     </div>
   )
 }
