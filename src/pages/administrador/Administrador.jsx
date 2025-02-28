92672d14ae55f962f664fa338906529022705e71
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-02-28 09:12:22 -0300

    agregue el componete TablaProdyctos dentro del componente Administrador

--- src/pages/administrador/Administrador.jsx
+++ src/pages/administrador/Administrador.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ItemProducto from'../../components/productos/itemProducto/ItemProducto'
 import { Link } from 'react-router-dom'
 import { Table } from 'react-bootstrap'
-
-
+import TablaProductos from'../../components/productos/tablaProductos/TablaProductos'
 
 const Administrador = () => {
   return (
     <div className=''>
-      
+      <TablaProductos></TablaProductos>
     </div>
   )
 }
