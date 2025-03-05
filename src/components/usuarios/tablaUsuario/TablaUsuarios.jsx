c51487392223bd43b56985addf5cf6addc6cfc57
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-03-05 09:21:30 -0300

    agregue el maquetado necesario para el componente TablaUsuarios

--- src/components/usuarios/tablaUsuario/TablaUsuarios.jsx
+++ src/components/usuarios/tablaUsuario/TablaUsuarios.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
+
 
 const TablaUsuarios = () => {
   return (
     <div>
-      <h1>Usuarios</h1>
-      
+
+      <section className="container mainSection">
+        <div className="d-flex justify-content-between align-items-center mt-5 py-3">
+          <h1 className="display-6 fw-bold">Gestionar Usuarios</h1>
+
+          <i className="bi bi-file-earmark-plus fs-4"></i>
+
+        </div>
+        <hr />
+        <Table responsive bordered hover>
+          <thead>
+            <tr className="text-center">
+              <th>ID</th>
+              <th>Nombre Completo</th>
+              <th>Email</th>
+              <th>Rol</th>
+              <th>Estado De Cuenta</th>
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+
+
+          </tbody>
+        </Table>
+      </section>
     </div>
   )
 }
