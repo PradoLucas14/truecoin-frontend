a3fc77934e31e1d14a596cf671d8848bd81f5f39
Prado Brian Nicolas <nicolas88prado@gmail.com>
2025-03-05 09:26:09 -0300

    agregue el maquetado necesario para el componente ItemUsuario

--- src/components/usuarios/itemUsuario/ItemUsuario.jsx
+++ src/components/usuarios/itemUsuario/ItemUsuario.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const ItemUsuario = () => {
   return (
-    <div>
-      
-    </div>
+    <tr>
+      <td className="text-center">1</td>
+      <td>Prado Brian</td>
+      <td className="">$nicolas@gmail.com</td>
+      <td>Usuario</td>
+      <td className="text-center">
+        True
+      </td>
+      <td className="text-center">
+        <button className="btn m-1 btnOpciones">
+          <i className="bi bi-pencil-square"></i>
+        </button>
+        <button className="btn btnOpciones">
+          <i className="bi bi-trash" ></i>
+        </button>
+      </td>
+    </tr>
   )
 }
 
